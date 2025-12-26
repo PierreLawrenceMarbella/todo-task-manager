357539fa265f19bd6254ab31a68a902c0c00c1ce
PierreLawrenceMarbella <pierremarbella04@gmail.com>
2025-12-26 09:32:42 +0800

    add task model

---
+++ models/Task.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  priority: {
+    type: String,
+    enum: ["Low", "Medium", "High"],
+    default: "Low",
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Task", TaskSchema);
